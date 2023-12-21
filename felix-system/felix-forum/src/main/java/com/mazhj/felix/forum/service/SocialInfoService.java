b74fa602cc63f3b4f47601e2168d314e5390fb8f
mazhj <mazhj180@163.com>
2023-12-21 17:55:00 +0800

    :sparkles:引入用户社交关系

---
+++ felix-system/felix-forum/src/main/java/com/mazhj/felix/forum/service/SocialInfoService.java
@@ -0,0 +1,7 @@
+package com.mazhj.felix.forum.service;
+
+/**
+ * @author mazhj
+ */
+public interface SocialInfoService {
+}
---
+++ felix-system/felix-forum/src/main/java/com/mazhj/felix/forum/service/impl/SocialInfoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.mazhj.felix.forum.service.impl;
+
+import com.mazhj.felix.forum.service.SocialInfoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author mazhj
+ */
+@Service
+public class SocialInfoServiceImpl implements SocialInfoService {
+}
