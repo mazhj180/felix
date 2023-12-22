3153f6cf402548f854a678007bbea9ed70248f7d
mazhj <mazhj180@163.com>
2023-12-22 15:57:24 +0800

    :sparkles:新增话题缓存key builder

--- felix-common/felix-common-redis/src/main/java/com/mazhj/common/redis/keys/KeyBuilder.java
+++ felix-common/felix-common-redis/src/main/java/com/mazhj/common/redis/keys/KeyBuilder.java
@@ -8,50 +8,58 @@ import com.mazhj.common.core.enums.ServiceModel;
 public final class KeyBuilder {
 
     public static class User{
-        private static final String prefix = ServiceModel.USER.str();
+        private static final String PREFIX = ServiceModel.USER.str();
     }
 
     public static class Book{
-        private static final String prefix = ServiceModel.BOOK.str();
+        private static final String PREFIX = ServiceModel.BOOK.str();
 
         public static String getBookDetailKey(String bookId){
-            return String.format(prefix + "detail:key-%s",bookId);
+            return String.format(PREFIX + "detail:key-%s",bookId);
         }
 
         public static String getChaptersKey(String bookId){
-            return String.format(prefix + "chapters:key-%s",bookId);
+            return String.format(PREFIX + "chapters:key-%s",bookId);
         }
 
         public static String getChapterKey(String booId){
-            return String.format(prefix + "chapter:key-%s",booId);
+            return String.format(PREFIX + "chapter:key-%s",booId);
         }
 
         public static String getChapterNodeKey(String bookId){
-            return String.format(prefix + "chapter-node:key-%s",bookId);
+            return String.format(PREFIX + "chapter-node:key-%s",bookId);
         }
 
         public static String getBookCategoriesKey(String bookId){
-            return String.format(prefix + "book-categories:key-%s",bookId);
+            return String.format(PREFIX + "book-categories:key-%s",bookId);
         }
     }
 
     public static class Homepage{
-        private static final String prefix = ServiceModel.HOMEPAGE.str();
+        private static final String PREFIX = ServiceModel.HOMEPAGE.str();
 
         public static String getHotRankingsKey(){
-            return prefix + "hot-rankings:key";
+            return PREFIX + "hot-rankings:key";
         }
 
         public static String getLikeRankingsKey(){
-            return prefix + "like-rankings:key";
+            return PREFIX + "like-rankings:key";
         }
 
         public static String getPossibleLikeKey(){
-            return prefix + "possible-like:key";
+            return PREFIX + "possible-like:key";
         }
 
         public static String getReasonKey(){
-            return prefix + "guess-you-reason";
+            return PREFIX + "guess-you-reason:key";
+        }
+    }
+
+    public static class Forum{
+        private static final String PREFIX = ServiceModel.FORUM.str();
+
+        public static String getTopicRemarkKey(String topicId){
+            return String.format(PREFIX + "topic-remark:key-%s",topicId);
         }
     }
 
